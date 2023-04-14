f1bf657d60517fd6ebab5a62716134699298393d
CSBonLaboratory <74656658+CSBonLaboratory@users.noreply.github.com>
2023-04-14 23:03:08 +0300

    Update coverity_upload.sh

--- coverity_upload.sh
+++ coverity_upload.sh
@@ -5,6 +5,6 @@ tar zxvf coverity_tool.tgz
 export UK_WORKDIR=/home/runner/work/UnikraftCoverity/UnikraftCoverity
 export UK_ROOT=/home/runner/work/UnikraftCoverity/UnikraftCoverity/unikraft
 export UK_LIBS=/home/runner/work/UnikraftCoverity/UnikraftCoverity/libs
-../../cov-analysis-linux64-2022.6.0/bin/cov-build --dir cov-int make
+./cov-analysis-linux64-2022.6.0/bin/cov-build --dir cov-int make
 tar czvf analysis_input.tgz cov-int
 curl --form _5PXI3ZAJ4wN2hGlCdZhJA --form email=csbon420@protonmail.com --form file=@./apps/app-helloworld/analysis_input.tgz --form version=1 --form description="Using github actions first try" https://scan.coverity.com/builds?project=Unikraft-Scanning
