f05276ce8431f67ae0727b7a311d25ad489bb20a
CSBonLaboratory <74656658+CSBonLaboratory@users.noreply.github.com>
2023-06-11 19:08:49 +0300

    Changed UK env vars to UnikraftScanning

--- coverity_upload.sh
+++ coverity_upload.sh
@@ -2,9 +2,9 @@
 cd ./apps/app-helloworld
 wget https://scan.coverity.com/download/linux64 --post-data "token=_5PXI3ZAJ4wN2hGlCdZhJA&project=Unikraft-Scanning" -O coverity_tool.tgz
 tar zxvf coverity_tool.tgz
-export UK_WORKDIR=/home/runner/work/UnikraftCoverity/UnikraftCoverity
-export UK_ROOT=/home/runner/work/UnikraftCoverity/UnikraftCoverity/unikraft
-export UK_LIBS=/home/runner/work/UnikraftCoverity/UnikraftCoverity/libs
+export UK_WORKDIR=/home/runner/work/UnikraftScanning/UnikraftScanning
+export UK_ROOT=/home/runner/work/UnikraftScanning/UnikraftScanning/unikraft
+export UK_LIBS=/home/runner/work/UnikraftScanning/UnikraftScanning/libs
 ./cov-analysis-linux64-2022.6.0/bin/cov-build --dir cov-int make
 tar czvf analysis_input.tgz cov-int
 curl --form token=_5PXI3ZAJ4wN2hGlCdZhJA --form email=csbon420@protonmail.com --form "file=@./analysis_input.tgz" --form version=1 --form description="Using github actions first try" https://scan.coverity.com/builds?project=Unikraft-Scanning
