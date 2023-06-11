04db89f6a4bdcd1f3c7664581f9aa4aabb6f7f85
CSBonLaboratory <74656658+CSBonLaboratory@users.noreply.github.com>
2023-06-11 20:20:32 +0300

    cov-analysis-linux64-.... changed in coverity_upload.sh
    This exception needs to be address in the future !!!

--- coverity_upload.sh
+++ coverity_upload.sh
@@ -6,6 +6,6 @@ export UK_WORKDIR=/home/runner/work/UnikraftScanning/UnikraftScanning
 export UK_ROOT=/home/runner/work/UnikraftScanning/UnikraftScanning/unikraft
 export UK_LIBS=/home/runner/work/UnikraftScanning/UnikraftScanning/libs
 ls -lrt
-./cov-analysis-linux64-2022.6.0/bin/cov-build --dir cov-int make
+./cov-analysis-linux64-2022.12.2/bin/cov-build --dir cov-int make
 tar czvf analysis_input.tgz cov-int
 curl --form token="$COVERITY_UPLOAD_TOKEN" --form email=csbon420@protonmail.com --form "file=@./analysis_input.tgz" --form version=1 --form description="Submited via Github action" https://scan.coverity.com/builds?project=Unikraft-Scanning
