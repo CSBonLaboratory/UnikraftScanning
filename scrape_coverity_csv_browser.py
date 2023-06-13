9575eb13e0ea64aa3f49d2d82a5f2da7c5e67cf6
CSBonLaboratory <74656658+CSBonLaboratory@users.noreply.github.com>
2023-06-13 19:06:39 +0300

    changed to element to be clickable everywhere

--- scrape_coverity_csv_browser.py
+++ scrape_coverity_csv_browser.py
@@ -72,14 +72,14 @@ try:
 
     time.sleep(5)
 
-    WebDriverWait(browser,20).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/nav/div[1]/div/div[1]/ul/li[3]/a')))
+    WebDriverWait(browser,20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/nav/div[1]/div/div[1]/ul/li[3]/a')))
     outstanding_issues = browser.find_element(By.XPATH, '/html/body/div[2]/nav/div[1]/div/div[1]/ul/li[3]/a')
     action.move_to_element(outstanding_issues)
     print("Hover mouse over Outstanding Defects tab OK")
 
     time.sleep(5)
 
-    WebDriverWait(browser,20).until(EC.visibility_of_element_located((By.XPATH,'/html/body/div[2]/nav/div[1]/div/div[1]/ul/li[3]/a/span')))
+    WebDriverWait(browser,20).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[2]/nav/div[1]/div/div[1]/ul/li[3]/a/span')))
     browser.find_element(By.XPATH,'/html/body/div[2]/nav/div[1]/div/div[1]/ul/li[3]/a/span').click()
     print("Click on more options in Outstanding Defects OK")
 
